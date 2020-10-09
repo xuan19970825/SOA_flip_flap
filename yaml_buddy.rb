c205bca6b51d5f03550c05c6509cca4372bf7838
林雨萱 <linyuxuan@linyuxuandeMacBook-Pro.local>
2020-10-09 10:24:34 +0800

    Reduce flog score

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -7,30 +7,48 @@ module YamlBubby
   def take_yaml(yaml)
     @data = ''
     survey = YAML.safe_load(yaml)
-    first_hash = survey[0]
-    keys_array = first_hash.keys
-    @data << keys_array.join("\t")
-    @data << "\n"
+    join(survey)
+    @data
+  end
+
+  def join(survey)
+    join_key(survey)
     survey.each do |record|
       @data << record.values.join("\t")
       @data << "\n"
     end
-    @data
+  end
+
+  def join_key(survey)
+    keys_array = survey[0].keys
+    @data << keys_array.join("\t")
+    @data << "\n"
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_yaml
-    survey = []
-    keys = @data[0].split("\t")
-    keys.map!(&:chomp)
+    split.to_yaml
+  end
+
+  def split
+    keys = prepare_key
     @data.shift
+    survey = []
     @data.each do |line|
-      values = line.split("\t")
-      record = {}
-      keys.each_index { |index| record[keys[index]] = values[index].chomp }
-      survey.push(record)
+      survey.push(split_line_by(line, keys))
     end
-    survey.to_yaml
+    survey
+  end
+
+  def prepare_key
+    @data[0].split("\t").map!(&:chomp)
+  end
+
+  def split_line_by(line, keys)
+    values = line.split("\t")
+    record = {}
+    keys.each_index { |index| record[keys[index]] = values[index].chomp }
+    record
   end
 end
